4e091da0e61f2955adcdaeb9c14d4e79f269fd08
read-quran <alomar3363@gmail.com>
2025-01-03 03:30:39 +0400

    Update script.js

--- script.js
+++ script.js
@@ -567,3 +567,19 @@ ${currentMode === "hizb" ? "الأحزاب" : "الأجزاء"} المتبقية
     updateInputLimits();
     loadData();
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful');
+      })
+      .catch(err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
+window.addEventListener('offline', () => {
+  alert('التطبيق يعمل دون اتصال بالإنترنت.');
+});
